10c64e211d5d9f4d5b84a9af5cb7ad5b898f704b
kishan kanejiya <16mscit084@gmail.com>
2020-07-06 19:06:09 +0530

    Updates

---
+++ precache-manifest.7fbe71258a0778f9abfb366a8a03e8bc.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb8fb4c0d5c1e81255c82c8a6189b302",
+    "url": "/kishankanejiya/TechSolutions/index.html"
+  },
+  {
+    "revision": "938c26ed6d172ad94ca0",
+    "url": "/kishankanejiya/TechSolutions/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "56d08ca3b507f98e113d",
+    "url": "/kishankanejiya/TechSolutions/static/css/main.ffebdc35.chunk.css"
+  },
+  {
+    "revision": "938c26ed6d172ad94ca0",
+    "url": "/kishankanejiya/TechSolutions/static/js/2.a4b7fb10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kishankanejiya/TechSolutions/static/js/2.a4b7fb10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56d08ca3b507f98e113d",
+    "url": "/kishankanejiya/TechSolutions/static/js/main.0f554fcd.chunk.js"
+  },
+  {
+    "revision": "4a9265bc5bbaab68df8d",
+    "url": "/kishankanejiya/TechSolutions/static/js/runtime-main.b99f3938.js"
+  },
+  {
+    "revision": "d3197ea20f7362aebac80a191200f2f4",
+    "url": "/kishankanejiya/TechSolutions/static/media/Docker.d3197ea2.d3197ea2.png"
+  },
+  {
+    "revision": "c9f8905823e90cf996c84c64ff4c53db",
+    "url": "/kishankanejiya/TechSolutions/static/media/Git.c9f89058.c9f89058.png"
+  },
+  {
+    "revision": "1c660651a220431323c6033094b80237",
+    "url": "/kishankanejiya/TechSolutions/static/media/Mongodb.1c660651.1c660651.png"
+  },
+  {
+    "revision": "57c666e64bab60d06ba4b806143f0749",
+    "url": "/kishankanejiya/TechSolutions/static/media/Mysql.57c666e6.57c666e6.png"
+  },
+  {
+    "revision": "b64f97310d8228fb2e801419d332f2d3",
+    "url": "/kishankanejiya/TechSolutions/static/media/Vuejs.b64f9731.b64f9731.png"
+  },
+  {
+    "revision": "8d431ec54df3a5dd744a4bcccedeb8a0",
+    "url": "/kishankanejiya/TechSolutions/static/media/abstract-art-blur-bright-373543.8d431ec5.jpg"
+  },
+  {
+    "revision": "7ee535517dc8615d4f044f2f79c9f315",
+    "url": "/kishankanejiya/TechSolutions/static/media/apple-laptop-notebook-office-39284.7ee53551.jpg"
+  },
+  {
+    "revision": "69d7257cfb332f7a3397f2c9a9f1e446",
+    "url": "/kishankanejiya/TechSolutions/static/media/aws.69d7257c.69d7257c.png"
+  },
+  {
+    "revision": "8e2034c0fdde7a174971216806b82658",
+    "url": "/kishankanejiya/TechSolutions/static/media/internet-screen-security-protection-60504.8e2034c0.jpg"
+  },
+  {
+    "revision": "2e427ed7f17a236de8ed7cb42e11e883",
+    "url": "/kishankanejiya/TechSolutions/static/media/react.2e427ed7.2e427ed7.png"
+  },
+  {
+    "revision": "c4f5c857152fb9eac537d002e938d487",
+    "url": "/kishankanejiya/TechSolutions/static/media/socket.c4f5c857.c4f5c857.png"
+  },
+  {
+    "revision": "811f7c90548b6aba774a8749d8fed15a",
+    "url": "/kishankanejiya/TechSolutions/static/media/white-and-black-laptop-computer-907489.811f7c90.jpg"
+  }
+]);
